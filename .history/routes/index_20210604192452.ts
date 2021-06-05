6c070a3cb14a4891209dfab077d5af19e1cf57f3
Viktoriia Romaniuk <vromaniu@my.centennialcollege.ca>
2021-06-04 20:42:20 -0400

    added partials

---
+++ .history/routes/index_20210604192452.ts
@@ -0,0 +1,37 @@
+/* index.ts : Viktoriia Romaniuk : 301079072 : 2021-06-03 */
+
+import express from 'express';
+const router = express.Router();
+export default router;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Viktoriia' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Viktoriia' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About' });
+});
+
+/* GET projects page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Projects' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+//module.exports = router;
