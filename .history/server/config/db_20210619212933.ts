d6b6d65c434cc19631615cbe8f318f9056cb7042
Viktoriia Romaniuk <vromaniu@my.centennialcollege.ca>
2021-06-19 21:40:42 -0400

    Updated access to remote MongoDB

--- .history/server/config/db_20210619212933.ts
+++ .history/server/config/db_20210619212933.ts
@@ -5,6 +5,6 @@ module.exports =
 }
 */
 export const LocalURI = "mongodb://localhost/users";
-export const RemoteURI = "mongodb+srv://romviki:cTNCS5vRME1eN9m0@cluster0.dp9xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+export const RemoteURI = "mongodb+srv://romviki:cTNCS5vRME1eN9m0@cluster0.lpnm6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 export const HostName = "RemoteHost";
 export const Secret = "someSecret";
