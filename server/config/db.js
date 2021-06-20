a22f54c306c1fe759f5f178c5d5efedf8aa4cda7
Viktoriia Romaniuk <vromaniu@my.centennialcollege.ca>
2021-06-19 21:28:02 -0400

    Updated access to remote MongoDB

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Secret = exports.HostName = exports.RemoteURI = exports.LocalURI = void 0;
 exports.LocalURI = "mongodb://localhost/users";
-exports.RemoteURI = "mongodb+srv://romviki:cTNCS5vRME1eN9m0@cluster0.dp9xm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+exports.RemoteURI = "mongodb+srv://romviki:<password>@cluster0.lpnm6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 exports.HostName = "RemoteHost";
 exports.Secret = "someSecret";
 //# sourceMappingURL=db.js.map
